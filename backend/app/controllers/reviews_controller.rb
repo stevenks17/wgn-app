4536cb01ed3e63608a8c64e828e06f4e18a71c56
stevenks17 <stevenks17@gmail.com>
2020-04-15 16:54:28 -0400

    3rd commit(API Controllers and Seeded data added)

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+    before_action :set_params, only: [:show, :update, :destroy]
+
+    def index
+        @games = Review.all
+        render json: @reviews, status: 200
+    end
+
+    def create
+        @review = Review.create(review_params)
+        render json: @review, status: 200
+    end
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:body, :gamer, :game_id)
+    end
+
+    def set_params
+        @review = Review.find(params[:id])
+    end
+
+
 end
